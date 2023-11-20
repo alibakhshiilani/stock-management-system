b2d7a802a66f9c8f459d407a6baba8d657bcf6c7
Ali Bakhshi Ilani <60128901+alibakhshiilani@users.noreply.github.com>
2023-11-20 15:14:50 +0330

    Update driver.js

--- api/routes/driver.js
+++ api/routes/driver.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { createDriver, getAllDrivers, getDriverById, updateDriver, deleteDriver } = require('../controllers/driverController');
 const adminMiddleware = require('./../middleware/adminMiddleware');
+const authMiddleware = require('./../middleware/authMiddleware');
 
 router.post('/',adminMiddleware,createDriver);
   
-router.get('/', adminMiddleware,getAllDrivers);
+router.get('/', authMiddleware,getAllDrivers);
   
 // router.get('/:id', getDriverById);
   
